0eb89fad409248e9c00993a7ba1fdf1438a279af
Justin <justinnaja01@gmail.com>
2026-08-24 11:20:16 +0700

    Lab002 Update

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID :1690700446
+ * Name       :Nathanon Boonkongkird
+ * Section    :129A
+ * No.        :N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+
+
+namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+
+            int hePercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent:{hePercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damge!");
         }
     }
 }
